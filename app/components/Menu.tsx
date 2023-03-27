e4f92a79137f0c3bbd5c6626cf3a5f58ceba3daa
Mark Malström <mark@malstrom.me>
2023-03-27 18:53:33 -0500

    Add data model to Menu & make it generic

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,47 +1,68 @@
-import { Card } from "./Card"
+import { Card, VStack } from "./Card"
 
-export function Menu() {
+export namespace Menu {
+    export interface Model {
+        title?: string
+        items: Item[]
+    }
+
+    export interface Item {
+        imageUrl: string
+        imageAlt?: string
+        course: string
+        dish: string
+        description: string
+    }
+}
+
+export const eggsBenidictMenu: Menu.Model = {
+    items: [
+        {
+            imageUrl: "nectarine-toast.jpg",
+            course: "Hors d'oeuvre",
+            dish: "Nectarine Bruschetta",
+            description: "Honey, ricotta, and sliced nectarines on thick sliced country bread.",
+        },
+        {
+            imageUrl: "eggs-benny.jpg",
+            course: "Entrée",
+            dish: "Eggs Benny",
+            description: `Classic eggs Benedict with Hollandaise, ham, and poached eggs on fresh 
+            English muffins. Served with homefries and an arugula lemon salad.`,
+        },
+    ],
+}
+
+export function Menu({ menu }: { menu: Menu.Model }) {
     return (
-        <Card className="gap-6 py-2">
-            <h2 className="font-serif text-4xl font-extrabold capitalize text-gray-800">Menu</h2>
-
-            <div className="flex flex-col gap-12">
-                <div className="flex flex-col items-center gap-6">
-                    <img
-                        alt=""
-                        className="h-64 w-full rounded border border-black/25 object-cover"
-                        src="nectarine-toast.jpg"
-                    />
-                    <div className="flex flex-col items-center gap-2 divide-y divide-gray-900/10 text-center">
-                        <h3 className="w-full pt-2 font-serif text-3xl text-orange-700">
-                            Hors d&apos;oeuvre
-                        </h3>
-                        <span className="w-full pt-2 text-lg font-light uppercase text-gray-500">
-                            Nectarine Bruschetta
-                        </span>
-                        <span className="w-full pt-2 text-sm text-gray-800">
-                            Honey, ricotta, and sliced nectarines on thick sliced country bread.
-                        </span>
-                    </div>
+        <Card>
+            <VStack className="gap-6 py-2">
+                <h2 className="font-serif text-4xl font-extrabold capitalize text-gray-800">
+                    {menu.title ?? "Menu"}
+                </h2>
+                <div className="flex flex-col gap-12">
+                    {menu.items.map(item => (
+                        <div className="flex flex-col items-center gap-6" key={item.dish}>
+                            <img
+                                alt={item.imageAlt ?? ""}
+                                className="inner-border h-64 w-full rounded object-cover"
+                                src={item.imageUrl}
+                            />
+                            <div className="flex flex-col items-center gap-2 divide-y divide-gray-900/10 text-center">
+                                <h3 className="w-full pt-2 font-serif text-3xl text-orange-700">
+                                    {item.course}
+                                </h3>
+                                <span className="w-full pt-2 text-lg font-light uppercase text-gray-500">
+                                    {item.dish}
+                                </span>
+                                <span className="w-full pt-2 text-sm text-gray-800">
+                                    {item.description}
+                                </span>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                <div className="flex flex-col items-center gap-6">
-                    <img
-                        alt=""
-                        className="h-64 w-full rounded border border-black/25 object-cover"
-                        src="eggs-benny.jpg"
-                    />
-                    <div className="flex flex-col items-center gap-2 divide-y divide-gray-900/10 text-center">
-                        <h3 className="w-full pt-2 font-serif text-3xl text-orange-700">Entrée</h3>
-                        <span className="w-full pt-2 text-lg font-light uppercase text-gray-500">
-                            Eggs Benny
-                        </span>
-                        <span className="w-full pt-2 text-sm text-gray-800">
-                            Classic eggs Benedict with Hollandaise, ham, and poached eggs on fresh
-                            English muffins. Served with homefries and an arugula lemon salad.
-                        </span>
-                    </div>
-                </div>
-            </div>
+            </VStack>
         </Card>
     )
 }
