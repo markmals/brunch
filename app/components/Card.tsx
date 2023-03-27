0f69812b0c5665e42b13ee82c7104b82429f34a4
Mark Malström <mark@malstrom.me>
2023-03-27 18:53:02 -0500

    Add layout components & simplify card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -5,21 +5,45 @@ export function Card({ className, children }: { className?: string; children: Re
     return (
         <div
             className={classNames(
-                // className,
-                "border border-gray-200 bg-white px-4 py-5 shadow-sm sm:rounded-lg sm:px-6"
+                className,
+                /p-/.test(className ?? "") ||
+                    /px-/.test(className ?? "") ||
+                    /py-/.test(className ?? "") ||
+                    /pl-/.test(className ?? "") ||
+                    /pr-/.test(className ?? "") ||
+                    /pt-/.test(className ?? "") ||
+                    /pb-/.test(className ?? "")
+                    ? null
+                    : "px-4 py-5 sm:px-6",
+                "border border-gray-200 bg-white shadow-sm sm:rounded-lg"
             )}
         >
-            <div
-                className={classNames(
-                    className,
-                    /justify/.test(className ?? "") ? null : "justify-center",
-                    /items/.test(className ?? "") ? null : "items-center",
-                    /flex/.test(className ?? "") ? null : "flex-col",
-                    "flex flex-wrap sm:flex-nowrap"
-                )}
-            >
-                {children}
-            </div>
+            {children}
+        </div>
+    )
+}
+
+function stackClasses(className?: string): (string | null)[] {
+    return [
+        /justify/.test(className ?? "") ? null : "justify-center",
+        /items/.test(className ?? "") ? null : "items-center",
+        /gap/.test(className ?? "") ? null : "gap-1",
+        "flex",
+    ]
+}
+
+export function VStack({ className, children }: { className?: string; children: ReactNode }) {
+    return (
+        <div className={classNames(className, ...stackClasses(className), "flex-col")}>
+            {children}
+        </div>
+    )
+}
+
+export function HStack({ className, children }: { className?: string; children: ReactNode }) {
+    return (
+        <div className={classNames(className, ...stackClasses(className), "flex-row")}>
+            {children}
         </div>
     )
 }
