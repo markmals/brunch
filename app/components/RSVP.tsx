d7e9ced945b2e6f2e673ec1d52af0f2011bb3690
Mark Malström <mark@malstrom.me>
2023-03-29 23:43:32 -0500

    Add pending UI

--- app/components/RSVP.tsx
+++ app/components/RSVP.tsx
@@ -1,9 +1,9 @@
 import { RadioGroup } from "@headlessui/react"
 import { CheckIcon, QuestionMarkCircleIcon, XMarkIcon } from "@heroicons/react/24/outline"
 import { CheckCircleIcon } from "@heroicons/react/24/solid"
-import type { User } from "@prisma/client"
-import { Response } from "@prisma/client"
-import { Form } from "@remix-run/react"
+import type { User } from "@prisma/client/edge"
+import { Response } from "@prisma/client/edge"
+import { Form, useNavigation } from "@remix-run/react"
 import type { ForwardRefExoticComponent, SVGProps } from "react"
 import { useEffect, useMemo, useState } from "react"
 import { capitalize } from "~/utilities/capitalize"
@@ -11,6 +11,8 @@ import { classNames } from "~/utilities/class-names"
 import { Card } from "./Card"
 
 export function Rsvp({ user }: Rsvp.Props) {
+    let navigation = useNavigation()
+
     let [selectedResponse, setSelectedResponse] = useState<Response | null>(user.response)
 
     let [name, setName] = useState(user.name)
@@ -75,16 +77,16 @@ export function Rsvp({ user }: Rsvp.Props) {
                 <>
                     <hr className="border-t border-black/10" />
 
-                    <div className="px-4 py-6 sm:px-6">
-                        <h3 className="text-base font-semibold leading-6 text-gray-900">
+                    <div className="py-6">
+                        <h3 className="px-6 text-base font-semibold leading-6 text-gray-900">
                             {title ?? "Unknown"}
                         </h3>
-                        <div className="mt-2 max-w-xl text-sm text-gray-500">
+                        <div className="mt-2 max-w-xl px-6 text-sm text-gray-500">
                             <p>{description ?? "Unknown"}</p>
                         </div>
                         <Form
                             action={`/${user.shortCode}`}
-                            className="mt-5 w-full sm:flex sm:items-center"
+                            className="mt-5 flex w-full flex-col items-end"
                             method="post"
                         >
                             <input
@@ -94,7 +96,7 @@ export function Rsvp({ user }: Rsvp.Props) {
                                 value={selectedResponse ?? ""}
                             />
 
-                            <div className="flex w-full flex-col divide-y divide-black/10">
+                            <div className="flex w-full flex-col divide-y divide-black/10 px-6">
                                 <div className="grid grid-rows-2 items-center border-t border-black/10 py-6 sm:grid-cols-2 sm:grid-rows-none">
                                     <label
                                         className="block text-sm font-medium leading-6 text-gray-900"
@@ -217,16 +219,46 @@ export function Rsvp({ user }: Rsvp.Props) {
                                         </div>
                                     </>
                                 )}
+                            </div>
 
-                                <div className="flex items-center justify-end gap-x-6 pb-2 pt-6">
-                                    <button
-                                        className="rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 enabled:bg-indigo-600 enabled:text-white enabled:shadow-sm enabled:hover:bg-indigo-500 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-500"
-                                        disabled={!isDirty}
-                                        type="submit"
-                                    >
-                                        {user.response ? "Update Response" : "Submit Response"}
-                                    </button>
-                                </div>
+                            <hr className="w-full border-t border-black/10" />
+
+                            <div className="flex items-center justify-end gap-x-6 px-6 pb-2 pt-6">
+                                <button
+                                    className="flex flex-row items-center justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 enabled:bg-indigo-600 enabled:text-white enabled:shadow-sm enabled:hover:bg-indigo-500 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-500"
+                                    disabled={!isDirty || navigation.state !== "idle"}
+                                    type="submit"
+                                >
+                                    {navigation.state !== "idle" && (
+                                        <div role="status">
+                                            <svg
+                                                aria-hidden="true"
+                                                className="mr-2 h-4 w-4 animate-spin fill-gray-500 text-gray-400"
+                                                fill="none"
+                                                viewBox="0 0 100 101"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                    fill="currentColor"
+                                                />
+                                                <path
+                                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                    fill="currentFill"
+                                                />
+                                            </svg>
+                                            <span className="sr-only">Loading...</span>
+                                        </div>
+                                    )}
+
+                                    {navigation.state !== "idle"
+                                        ? user.response
+                                            ? "Updating..."
+                                            : "Submitting..."
+                                        : user.response
+                                        ? "Update Response"
+                                        : "Submit Response"}
+                                </button>
                             </div>
                         </Form>
                     </div>
